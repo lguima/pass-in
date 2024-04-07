b48928477705b81c46418d30c3d95b7077023279
Lucas Guima <lucasguima.felipe@gmail.com>
2024-04-07 15:10:19 -0300

    Update Credential component

--- src/components/Credential.tsx
+++ src/components/Credential.tsx
@@ -1,5 +1,6 @@
-import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Image, ImageBackground, Text, TouchableOpacity, View, useWindowDimensions } from "react-native";
 import { Feather } from "@expo/vector-icons"
+import { MotiView } from 'moti'
 
 import { colors } from "@/styles/colors";
 import { BadgeStore } from "@/store/badge-store";
@@ -17,8 +18,34 @@ export function Credential({
   onChangeAvatar,
   onExpandQRCode
 }: Props) {
+  const { height } = useWindowDimensions();
+
   return (
-    <View className="w-full self-stretch items-center">
+    <MotiView
+      className="w-full self-stretch items-center"
+      from={{
+        opacity: 1,
+        translateY: -height,
+        rotateZ: "50deg",
+        rotateY: "30deg",
+        rotateX: "30deg",
+      }}
+      animate={{
+        opacity: 1,
+        translateY: 0,
+        rotateZ: "0deg",
+        rotateY: "0deg",
+        rotateX: "0deg",
+      }}
+      transition={{
+        type: "spring",
+        damping: 20,
+        rotateZ: {
+          damping: 15,
+          mass: 3,
+        },
+      }}
+    >
       <Image
         source={require("@/assets/ticket/band.png")}
         className="w-24 h-52 z-10"
@@ -84,6 +111,6 @@ export function Credential({
           </Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </MotiView>
   )
 }
