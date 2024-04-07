93adcf80880aba8b68dead06229d2a02fd0348c0
Lucas Guima <lucasguima.felipe@gmail.com>
2024-04-07 10:48:38 -0300

    Add Credential to Ticket screen

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,5 +1,6 @@
 import { StatusBar, View } from "react-native";
 
+import { Credential } from '@/components/Credential';
 import { Header } from '@/components/Header';
 
 export default function Ticket() {
@@ -8,6 +9,8 @@ export default function Ticket() {
       <StatusBar barStyle="light-content" />
 
       <Header title="Minha credencial" />
+
+      <Credential />
   </View>
   )
 }
