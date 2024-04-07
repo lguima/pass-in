23966c80b06d3ab95f0ab578f784a21417ad0ac3
Lucas Guima <lucasguima.felipe@gmail.com>
2024-04-07 11:37:59 -0300

    Update Credential component

--- src/components/Credential.tsx
+++ src/components/Credential.tsx
@@ -1,6 +1,13 @@
 import { Image, ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons"
+import { colors } from "@/styles/colors";
 
-export function Credential() {
+type Props = {
+  image?: string
+  onChangeAvatar?: () => void
+}
+
+export function Credential({ image, onChangeAvatar }: Props) {
   return (
     <View className="w-full self-stretch items-center">
       <Image
@@ -21,10 +28,29 @@ export function Credential() {
           <View className="w-40 h-40 bg-black rounded-full" />
         </ImageBackground>
 
-        <Image
-          source={{ uri: "https://github.com/lguima.png" }}
-          className="w-36 h-36 rounded-full -mt-24"
-        />
+        { image ? (
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={onChangeAvatar}
+          >
+            <Image
+              source={{ uri: image }}
+              className="w-36 h-36 rounded-full -mt-24"
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            activeOpacity={0.9}
+            className="w-36 h-36 rounded-full -mt-24 bg-gray-400 items-center justify-center"
+            onPress={onChangeAvatar}
+          >
+            <Feather
+              name="camera"
+              color={colors.green[400]}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
 
         <Text className="font-bold text-2xl text-zinc-50 mt-4">
           Lucas Guima
