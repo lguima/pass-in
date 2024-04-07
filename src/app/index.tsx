3900ec51fc3b1e0d892c117253bee184b9105021
Lucas Guima <lucasguima.felipe@gmail.com>
2024-04-07 10:10:26 -0300

    Create initial screen

---
+++ src/app/index.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { View, Image, StatusBar, Alert } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+
+import { colors } from '@/styles/colors';
+import { Input } from '@/components/Input';
+import { Button } from '@/components/Button'
+
+export default function Home() {
+  const [code, setCode] = useState('');
+
+  function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert('Ingresso', 'Informe o código do ingresso.');
+    }
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center p-8 bg-green-500">
+      <StatusBar barStyle="light-content" />
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={setCode}
+          />
+        </Input>
+
+        <Button
+          title="Acessar credencial"
+          onPress={handleAccessCredential}
+        />
+
+        <Link
+          href="/register"
+          className="text-gray-100 text-base text-center mt-8"
+        >
+          Ainda não possui ingresso?
+        </Link>
+      </View>
+    </View>
+  )
+}
